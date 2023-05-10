1dd2b0810514b6cf8b75d30467f19e357d07ef27
William Yang <william.yang@hashicorp.com>
2023-05-10 17:05:17 +0800

    .

--- app.py
+++ app.py
@@ -7,25 +7,24 @@ from dotenv import load_dotenv
 load_dotenv('var.env')
 
 # Get the Vault address and token from the environment variables
-vault_address = os.environ['vault_address']
-vault_token = os.environ['vault_token']
+vault_address = os.environ['VAULT_ADDRESS']
+vault_token = os.environ['VAULT_TOKEN']
 
 # Initialize the Vault client with the 'admin' namespace
 client = hvac.Client(url=vault_address, token=vault_token, namespace="admin")
 
-# ... rest of the code ...
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# ...
 @app.route('/read_secret', methods=['GET', 'POST'])
 def read_secret():
     if request.method == 'POST':
         base_path = request.form.get('path')
         first, second = base_path.split('/', 1)
-        secret_path = '{}/data/{}'.format(first, second)  # Insert 'data' between the first level path and the remaining
+        secret_path = '{}/data/{}'.format(first, second)
         try:
             secret_response = client.read(secret_path)
             secret_data = secret_response['data']['data']
@@ -44,22 +43,16 @@ def write_secret():
     if request.method == 'POST':
         base_path = request.form.get('path')
         first, second = base_path.split('/', 1)
-        secret_path = '{}/data/{}'.format(first, second)  # Insert 'data' between the first level path and the remaining
+        secret_path = '{}/data/{}'.format(first, second)
         key = request.form.get('key')
         value = request.form.get('value')
         try:
-            # Read the secret at the given path
             secret_response = client.read(secret_path)
-
-            # If the secret exists, update the data dictionary with the new key-value pair
             if secret_response:
                 data = secret_response['data']['data']
                 data[key] = value
-            # If the secret does not exist, create a new data dictionary with the key-value pair
             else:
                 data = {key: value}
-
-            # Write the updated data dictionary back to Vault
             client.write(secret_path, data=data)
             return render_template('result.html', message="Secret written successfully.")
         except Exception as e:
@@ -72,8 +65,6 @@ def write_secret():
             <input type="submit" value="Write Secret">
         </form>
     '''
-# ...
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
